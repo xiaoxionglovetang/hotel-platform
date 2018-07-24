200b31b3152aa74b83b4bd27694116a24df463bd
xiongliyu <315696190@qq.com>
2018-07-24 18:28:30 +0800

    add two button components

---
+++ src/components/Home/Header/Base/ContrastButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <a class="login-btn">登录</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+@import (reference) '../../../../../static/css/base.less';
+
+.login-btn {
+  display: inline-block;
+  .size(70px, 28px);
+  border-radius: 3px;
+  border:1px solid @primary-border;
+  line-height: 28px;
+  .f-color-size(@primary-button-color, 14px);
+  text-align: center;
+  background-color: #fff;
+  &:hover {
+    color: #fff;
+    border-color:  @primary-button-hover;
+    background-color: @primary-button-hover;
+  }
+}
+</style>
---
+++ src/components/Home/Header/Base/GradientButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <a class="register-btn">注册</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+@import (reference) '../../../../../static/css/base.less';
+
+.register-btn {
+  display: inline-block;
+  .size(70px, 30px);
+  border-radius: 3px;
+  line-height: 30px;
+  .f-color-size(#fff, 14px);
+  text-align: center;
+  background-color: @primary-button-color;
+  &:hover {
+    background-color: @primary-button-hover;
+  }
+}
+</style>
